d121851693eec86c0790b87c68de9332835c89a8
yfsper233 <yfsper233@163.com>
2024-10-14 17:35:30 +0800

    init

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,6 +1,31 @@
 require("@nomicfoundation/hardhat-toolbox");
+require("dotenv").config();
 
-/** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
-  solidity: "0.8.27",
+  solidity: {
+    version: "0.8.24", 
+    settings: {
+      optimizer: {
+        enabled: true,
+        runs: 200
+      },
+      evmVersion: "paris"
+    }
+  },
+  networks: {
+    fuji: {
+      url: `https://avalanche-fuji.infura.io/v3/${process.env.INFURA_PROJECT_ID}`,
+      chainId: 43113,
+      accounts: [process.env.PRIVATE_KEY], 
+    },
+    localhost: {
+      url: "http://127.0.0.1:8545"
+    },
+  },
+  paths: {
+    sources: "./contracts",
+    tests: "./test",
+    cache: "./cache",
+    artifacts: "./artifacts"
+  }
 };
---
+++ scripts/deploy-realestate-token.js
@@ -0,0 +1,51 @@
+const hre = require("hardhat");
+const fs = require('fs');
+
+
+async function main() {
+    const RealEstateToken = await hre.ethers.getContractFactory("RealEstateToken");
+
+    // 构造函数参数
+    const uri = "";
+    const ccipRouterAddress = "0xF694E193200268f9a4868e4Aa017A0118C9a8177";
+    const linkTokenAddress = "0x0b9d5D9136855f6FEc3c0993feE6E9CE8a297846";
+    const currentChainSelector = "14767482510784806043";
+    const functionsRouterAddress = "0xA9d587a00A31A52Ed70D6026794a8FC5E2F5dCb0";
+
+    console.log("Deploying RealEstateToken...");
+    const realEstateToken = await RealEstateToken.deploy(
+        uri,
+        ccipRouterAddress,
+        linkTokenAddress,
+        currentChainSelector,
+        functionsRouterAddress
+    );
+
+    await realEstateToken.waitForDeployment()
+
+    console.log("RealEstateToken deployed to:", realEstateToken.target);
+
+    const deploymentInfo = {
+        contractName: "RealEstateToken",
+        address: realEstateToken.target,
+        network: hre.network.name,
+        timestamp: new Date().toISOString()
+    };
+
+    fs.writeFileSync(
+        'deployment-info.json',
+        JSON.stringify(deploymentInfo, null, 2)
+    );
+
+    console.log("Deployment info saved to deployment-info.json");
+}
+
+main()
+    .then(() => process.exit(0))
+    .catch((error) => {
+        console.error(error);
+        process.exit(1);
+    });
+
+
+
